a29ebe72d50a241a1da72c75b695ca462ee30c12
Sandra Pla Libourel <sandraplalibourel@gmail.com>
2021-02-03 21:35:34 +0100

    back contact form done

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
+     * @param Request $request
+     * @param MailerInterface $mailer
+     * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+
+            $email = (new Email())
+                ->from('e740c2b349-44616e@inbox.mailtrap.io')
+                ->to('e740c2b349-44616e@inbox.mailtrap.io')
+                ->subject('Demande de contact via portfolio')
+                ->text('Sender : '. $contactFormData['email'] . \PHP_EOL .
+                    $contactFormData['message']);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('contact');
+        }
+
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contact_form' => $form->createView()
         ]);
     }
 }
